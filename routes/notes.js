3773078a7508e82b32a525cb8e1681908b4a2930
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-14 04:45:35 -0400

    fixed a typo error

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ const notes = require('express').Router();
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 notes.get('/', (req, res) =>
-    readFromFile('./db/db.json'), then((data) => res.json(JSON.parse(data)))
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
 notes.post('/', (req, res) => {
